24cb3e8b97df33ab916b2b191246d5ad8424fbf3
VladV1V <mahno444@gmail.com>
2021-04-21 08:04:41 +0300

    sweet: build gapps

--- p404_sweet.mk
+++ p404_sweet.mk
@@ -9,7 +9,13 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit 404 common configuration.
 $(call inherit-product, vendor/404/configs/common.mk)
+
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
+
 TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := p404_sweet
